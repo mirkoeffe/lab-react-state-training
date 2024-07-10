69d3e1526b1009b0731b9a7888dad9c65687360a
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-10 16:42:31 +0200

    update counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,19 +5,19 @@ function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => {
     console.log("counter incremented");
-    setCount(count + 1);
+    setCount((prev) => prev + 1);
   };
   const decrement = () => {
     console.log("counter decremented");
     if (count > 0) {
-      setCount(count - 1);
+      setCount((prev) => prev - 1);
     }
   };
   return (
     <div>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={decrement}> - </button>
       <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>-</button>
+      <button onClick={increment}> + </button>
     </div>
   );
 }
