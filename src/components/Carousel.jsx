0d9465a6ef967b1ff78d6672a94440555a823cd1
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-11 10:12:01 +0200

    reroll for error typo

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 function Carousel({ images }) {
-  /*const [currentImageIndex, setImageIndex] = useState(0);
+  const [currentImageIndex, setImageIndex] = useState(0);
 
-   const handleLeftClick = () => {
+  const handleLeftClick = () => {
     setImageIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
@@ -20,28 +20,6 @@ function Carousel({ images }) {
       <button onClick={handleLeftClick}>Left</button>
       <img src={images[currentImageIndex]} alt="Carousel" />
       <button onClick={handleRightClick}>Right</button>
-    </div>
-  ); */
-
-  const [index, setIndex] = useState(0);
-  const images = [
-    "https://randomuser.me/api/portraits/women/1.jpg",
-    "https://randomuser.me/api/portraits/men/1.jpg",
-    "https://randomuser.me/api/portraits/women/2.jpg",
-    "https://randomuser.me/api/portraits/men/2.jpg",
-  ];
-  const handleClick = (direction) => {
-    if (direction === "left") {
-      setIndex((index - 1) % images.length);
-    } else {
-      setIndex((index + 1) % images.length);
-    }
-  };
-  return (
-    <div className="carousel">
-      <button onClick={() => handleClick("left")}>Left</button>
-      <img src={images[index]} />
-      <button onClick={() => handleClick("right")}>Right</button>
     </div>
   );
 }
